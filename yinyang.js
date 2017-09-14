7c4cfcd4319b767982277947accf90d0f58a4cab
fheyen <frank.heyen@outlook.de>
2017-09-14 21:46:19 +0200

    Update yinyang.js
    added raw class

--- yinyang.js
+++ yinyang.js
@@ -1,1 +1,30 @@
+class YinYang {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+    this.container = {};
+    this.canvas = {};
+  }
+  
+  get canvas() {
+    return this.canvas;
+  }
 
+  init() {
+    this.createContainer();
+    this.createCanvas();
+    this.draw();    
+  }
+  
+  createContainer() {
+  
+  }
+  
+  createCanvas() {
+  
+  }
+  
+  draw() {
+    
+  }
+}
