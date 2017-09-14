d4c4b461edaf6fef2c378e64deb5535a4dcbd0ad
fheyen <frank.heyen@outlook.de>
2017-09-15 01:43:45 +0200

    added comments

--- drawings/yinyang.js
+++ drawings/yinyang.js
@@ -1,5 +1,15 @@
-// TODO: comment
+/***
+* Usage:
+* Create a new object with   const yinyang = new YinYang(...);
+* Render it using            yinyang.init();
+*/
 class YinYang {
+  /***
+  * @param parent DOM elemnt to append this drawing to
+  * @param width (default: 100) width of the canvas
+  * @param height (default: 100) height of the canvas
+  * @param margin (default: 0) margin around image content
+  */
   constructor(parent, width = 100, height = 100, margin = 0) {
     this.title = "Yin Yang";
     this.width = width;
@@ -10,6 +20,9 @@ class YinYang {
     this.canvas = {};
   }
 
+  /***
+  * Creates the drawing container, title and canvas and draws the image.
+  */
   init() {
     this.clear();
     this.createContainer();
@@ -17,10 +30,17 @@ class YinYang {
     this.draw();
   }
 
+  /***
+  * Clears the parent element.
+  */
+  // TODO: only remove this.container
   clear() {
     this.parent.innerHTML = "";
   }
 
+  /***
+  * Creates the drawing container and title.
+  */
   createContainer() {
     this.container = document.createElement("div");
     this.container.className = "drawingContainer";
@@ -32,6 +52,9 @@ class YinYang {
     this.container.appendChild(this.heading);
   }
 
+  /***
+  * Creates the canvas.
+  */
   createCanvas() {
     this.canvas = document.createElement("canvas");
     this.canvas.width = this.width;
@@ -39,6 +62,9 @@ class YinYang {
     this.container.appendChild(this.canvas);
   }
 
+  /***
+  * Draws the image.
+  */
   draw() {
     const ctx = this.canvas.getContext("2d");
 
@@ -56,6 +82,15 @@ class YinYang {
     this.drawCircle(centerX + radius / 2, centerY, radius / 8, "#000");
   }
 
+  /***
+  * Draws a circle or partial circle.
+  * @param x x-cooridnate
+  * @param y y-coordinate
+  * @param radius radius
+  * @param fill fill color
+  * @param startAngle (default: 0) start angle
+  * @param endAngle (default: 2 * Pi) end angle
+  */
   drawCircle(x, y, radius, fill, startAngle = 0, endAngle = 2 * Math.PI) {
     const ctx = this.canvas.getContext("2d");
     ctx.fillStyle = fill;
